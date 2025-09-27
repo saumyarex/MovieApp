96d4483fced8b2693cce40d2e208c191a38b748d
Saumya Nayak <saumyo224@gmail.com>
2025-09-28 00:52:30 +0530

    refactor layout component to enhance tab icon rendering and improve styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,72 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
+
+const TabIcon = ({ title, icon, focused }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="w-full min-w-[112px] min-h-14 rounded-full overflow-hidden flex flex-row gap-2 p-2 items-center justify-center mt-2"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-base font-semibold">{title}</Text>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="w-full flex-row justify-center">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginBottom: 36,
+          marginHorizontal: 20,
+          height: 46,
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} title={"Home"} icon={icons.home} />
+          ),
         }}
       />
 
       <Tabs.Screen
         name="search"
         options={{
-          title: "search",
+          title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon title={"Search"} icon={icons.search} focused={focused} />
+          ),
         }}
       />
 
@@ -25,6 +75,9 @@ const _Layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon title={"Saved"} icon={icons.save} focused={focused} />
+          ),
         }}
       />
 
@@ -33,6 +86,9 @@ const _Layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon title={"Profile"} icon={icons.person} focused={focused} />
+          ),
         }}
       />
     </Tabs>
